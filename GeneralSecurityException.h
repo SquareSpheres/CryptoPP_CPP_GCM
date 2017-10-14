03483a643131394b1a05966bda678337685d3588
Yngve <yngvenot@gmail.com>
2017-10-14 02:46:27 +0200

    explicit constructors

--- GeneralSecurityException.h
+++ GeneralSecurityException.h
@@ -6,7 +6,7 @@ class GeneralSecurityException : public std::runtime_error
 {
 public:
 	GeneralSecurityException() :runtime_error("General security exception has occurred") {}
-	GeneralSecurityException(const std::string& what_arg) :runtime_error(what_arg) {}
+	explicit GeneralSecurityException(const std::string& what_arg) :runtime_error(what_arg) {}
 	~GeneralSecurityException() {}
 
 private:
--- IOException.h
+++ IOException.h
@@ -6,7 +6,7 @@ class IOException: public std::runtime_error
 {
 public:
 	IOException() :runtime_error("General I/O exception has occurred"){}
-	IOException(const std::string& what_arg):runtime_error(what_arg){}
+	explicit IOException(const std::string& what_arg):runtime_error(what_arg){}
 	~IOException(){}
 
 private:
